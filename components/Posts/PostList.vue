45661661a0ae7ab4ccc2674f73f7aa42df1ad054
1071sh <k.nishida@viatage-llc.com>
2021-01-07 11:15:40 +0900

    section3

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="post-list">
+        <PostPreview
+            :is-admin="isAdmin"
+            id="1"
+            title="Hello"
+            previewText="This my first post!"
+            thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        />
+        <PostPreview
+            :is-admin="isAdmin"
+            id="2"
+            title="Hello"
+            previewText="This my first post!"
+            thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        />
+        <PostPreview
+            :is-admin="isAdmin"
+            id="3"
+            title="Hello"
+            previewText="This my first post!"
+            thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        />
+    </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+    components: {
+        PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
+
+<style scoped>
+.post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
